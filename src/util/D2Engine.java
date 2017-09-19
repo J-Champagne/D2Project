6dae187ee6154d640a124f064ac903f01a5b568d
J-Champagne <slapjack.dev@gmail.com>
2017-09-19 11:24:27 -0400

    Initial Commit

---
+++ src/util/D2Engine.java
@@ -0,0 +1,11 @@
+package util;
+
+/**
+ * Created by J-Champagne on 9/19/17.
+ * Version: 0.1
+ * This project was created as an expirement for threading and JFrame graphics in java.
+ */
+
+public class D2Engine {
+
+}
